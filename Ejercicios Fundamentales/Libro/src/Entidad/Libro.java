2c196e19a0a7327781b7643f756708232ddd4521
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-11-23 21:51:39 -0300

    eje

---
+++ Ejercicios Fundamentales/Libro/src/Entidad/Libro.java
@@ -0,0 +1,19 @@
+
+package Entidad;
+
+
+public class Libro {
+   
+    private Long isbn;
+    private String titulo;
+    private Integer anio;
+    private Integer ejemplares;
+    private Integer ejemplaresPrestados;
+    private Integer ejemplaresRestantes;
+    private Boolean alta;
+    private Autor autor;
+            
+    
+    
+    
+}
