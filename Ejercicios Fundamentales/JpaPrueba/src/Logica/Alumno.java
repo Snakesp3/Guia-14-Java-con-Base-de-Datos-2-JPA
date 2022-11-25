20cbdb83871d1a8f0fb1dc26c8f619eafaa9fc17
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-11-24 22:01:14 -0300

    eje 01

--- Ejercicios Fundamentales/JpaPrueba/src/Logica/Alumno.java
+++ Ejercicios Fundamentales/JpaPrueba/src/Logica/Alumno.java
@@ -1,14 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Logica;
 
-/**
- *
- * @author Snakesp2
- */
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Alumno {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    @Basic
+    private String nombre;
+    private String apellido;
+    @Temporal(TemporalType.DATE)
+    private Date FechaNac;
+
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, String apellido, Date FechaNac) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.FechaNac = FechaNac;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNac() {
+        return FechaNac;
+    }
+
+    public void setFechaNac(Date FechaNac) {
+        this.FechaNac = FechaNac;
+    }
+    
+    
+    
+    
     
 }
