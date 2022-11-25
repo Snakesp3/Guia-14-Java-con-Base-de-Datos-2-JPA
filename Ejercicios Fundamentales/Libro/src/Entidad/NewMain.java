25695a14d1768ff2149b6d5ca0023ac9f216d50e
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-11-24 21:42:16 -0300

    ejemplos

---
+++ Ejercicios Fundamentales/Libro/src/Entidad/NewMain.java
@@ -0,0 +1,23 @@
+
+package Entidad;
+
+
+public class NewMain {
+
+    
+    public static void main(String[] args) {
+        
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
