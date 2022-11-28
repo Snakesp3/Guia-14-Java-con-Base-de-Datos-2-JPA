7ed273f2949a84a1405d32a3365682d953033416
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-11-28 16:50:49 -0300

    Repaso con pablito eje 1 fundamental JPA

--- Ejercicios Fundamentales/RepasoCon Pablito/src/Servicio/Serviciolibro.java
+++ Ejercicios Fundamentales/RepasoCon Pablito/src/Servicio/Serviciolibro.java
@@ -1,6 +1,5 @@
 package Servicio;
 
-
 import Entidades.Libro;
 import java.util.Scanner;
 import javax.persistence.EntityManager;
@@ -20,48 +19,57 @@ public class Serviciolibro {
     Scanner leer = new Scanner(System.in);
 
     Libro l1 = new Libro();
+    
     Servicioautor a1 = new Servicioautor();
     Servicioeditorial e1 = new Servicioeditorial();
 
     public void crearLibro() {
         try {
-            System.out.println("ingrese los datos de la Editorial a crear");
-            System.out.println("Ingrese el ISBN");
-            l1.setIsbn(leer.nextLong());
-            System.out.println("Ingrese el titulo");
-            l1.setTitulo(leer.next());
-            System.out.println("Ingrese el Anio");
-            l1.setAnio(leer.nextInt());
-            System.out.println("Ingrese el ejemplares");
-            l1.setEjemplares(leer.nextInt());
-            System.out.println("Ingrese el ejemplaresPrestados");
-            l1.setEjemplaresPrestados(leer.nextInt());
+            System.out.println("");
+            System.out.println("              *****************************************");
+            System.out.println("              ***INGRESE LOS DATOS DEL LIBRO A CREAR***");
+            System.out.println("              *****************************************");
+            System.out.println("");
+            System.out.println("<<<Ingrese el ISBN>>>");
+            System.out.print("->");l1.setIsbn(leer.nextLong());
+            System.out.println("*********************");
+            System.out.println("<<<Ingrese el Titulo>>>");
+            System.out.print("->");l1.setTitulo(leer.next());
+            System.out.println("***********************");
+            System.out.println("<<<Ingrese el Año>>>");
+            System.out.print("->");l1.setAnio(leer.nextInt());
+            System.out.println("********************");
+            System.out.println("<<<Cantidad de Ejemplares>>>");
+            System.out.print("->");l1.setEjemplares(leer.nextInt());
+            System.out.println("****************************");
+            System.out.println("<<<Cantidad de Ejemplares Prestados>>>");
+            System.out.print("->");l1.setEjemplaresPrestados(leer.nextInt());
+            System.out.println("**************************************");
             l1.setEjemplaresRestantes(l1.getEjemplares() - l1.getEjemplaresPrestados());
-            System.out.println("Ingrese el Id del autor");
-            l1.setAutor(a1.buscarAutorId(leer.nextInt()));
-             System.out.println("Ingrese el Id de a editorial");
-            l1.setEditorial(e1.buscarEditorialid(leer.nextInt()));
+            System.out.println("<<<Ingrese el Id del Autor>>>");
+            System.out.print("->");l1.setAutor(a1.buscarAutorId(leer.nextInt()));
+            System.out.println("*****************************");
+            System.out.println("<<<Ingrese el Id de la Editorial>>>");
+            System.out.print("->");l1.setEditorial(e1.buscarEditorialid(leer.nextInt()));
+            System.out.println("***********************************");
 
             em.getTransaction().begin();
-           //Ingresando a la tabla que voy a usar
+            //Ingresando a la tabla que voy a usar
             em.persist(l1);
             //Confirmar operacion
             em.getTransaction().commit();
         } catch (Exception e) {
             throw e;
         }
-
     }
+//Metodo Buscar libro
 
     public Libro buscarLibroIsbn(Long isbn) {
 
         try {
-
             return l1 = em.find(Libro.class, isbn);
-
         } catch (Exception e) {
             throw e;
         }
-
     }
 }
